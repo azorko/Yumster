010f0f11adab3aee80164a785ce5c64201730ae3
Anastasia Zorko <azorko13@gmail.com>
2014-12-02 15:05:23 -0800

    minor style edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,15 @@ Meal.create({tag: "Western", title: "French Breakfast", price: 20, max_guests: 1
 
 Meal.create({tag: "Western", title: "Philly Style", price: 30, max_guests: 10, host_id: 3, date: "2014-12-30", photo_url: "https://s3-us-west-1.amazonaws.com/yumster/philly-cheese.jpg", about: Faker::Lorem.paragraph(12)})
 
+Meal.create({tag: "Western", title: "Dessert for Dinner", price: 15, max_guests: 10, host_id: 1, date: "2014-12-27", photo_url: "https://s3-us-west-1.amazonaws.com/yumster/crepes.jpg", about: Faker::Lorem.paragraph(12)})
+
+
 User.create({email: "natasha@yumster.com", password: "secret", name: "Natasha", address: "3255 21st St, San Francisco, CA", lat: 37.75685199999999, lng: -122.42031359999999, photo_url: "https://s3-us-west-1.amazonaws.com/yumster/host1.jpg", about: Faker::Lorem.paragraph });
 
 User.create({email: "andrei@yumster.com", password: "secret", name: "Andrei", address: "429 Castro St, San Francisco, CA", lat: 37.7620333, lng: -122.43475910000001, photo_url: "https://s3-us-west-1.amazonaws.com/yumster/host2.jpg", about: Faker::Lorem.paragraph });
 
 User.create({email: "marina@yumster.com", password: "secret", name: "Marina", address: "1610 Geary Blvd, San Francisco, CA", lat: 37.7849934, lng: -122.43018119999999, photo_url: "https://s3-us-west-1.amazonaws.com/yumster/host3.jpg", about: Faker::Lorem.paragraph });
+
 
 3.times do |i|
   author_arr = [1, 2, 3]
